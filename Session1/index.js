d955193aee7ed22065240088093adf59a525da18
shubhamjain2211 <shubham.jain1@tothenew.com>
2020-04-28 19:18:05 +0530

    Updation in assignment1

--- Session1/index.js
+++ Session1/index.js
@@ -8,14 +8,16 @@ const http = require('http');
 const fs = require('fs');
 var server=http.createServer(function (req, response)
  {
+  console.log("Server is listening at PORT 2000.");
+  // console.log(response);
     fs.readFile('http.txt', function(err, content) 
     {
-            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.writeHead(500, {'Content-Type': 'text/html'});
             response.write(content);
             response.end();
   });
 })
 server.listen(2000);
-console.log("Server is listening at PORT 2000.");
+
 
 
